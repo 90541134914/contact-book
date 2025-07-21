c1efd2368de212b9d9431144b47ac03d67102afd
UnishGiri27 <unishgiri15@gmail.com>
2025-07-21 17:22:10 -0400

    Fix: reload contact list after adding contact

--- src/views/ContactList.vue
+++ src/views/ContactList.vue
@@ -1,29 +1,41 @@
+<template>
+  <div>
+    <h1>Contact List</h1>
+    <div style="display: flex; align-items: center; gap: 0.5rem; max-width: 400px; margin-bottom: 1rem;">
+      <input
+        v-model="search"
+        placeholder="Search contacts by first or last name"
+        type="text"
+      />
+      <button v-if="search" @click="search = ''">Clear</button>
+    </div>
+    <p v-if="filteredContacts.length === 0">No contacts found.</p>
+    <ul>
+      <li v-for="contact in filteredContacts" :key="contact.id">
+        <router-link :to="`/contact/${contact.id}`">
+          {{ contact.lastName }}, {{ contact.firstName }}
+        </router-link>
+      </li>
+    </ul>
+    <router-link to="/new">Add New Contact</router-link>
+  </div>
+</template>
+
 <script>
-import { ref, computed } from 'vue'
-import { useRoute, onBeforeRouteUpdate, onBeforeRouteEnter } from 'vue-router'
+import { ref, computed, watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
 import { getContacts } from '../utils/contactStorage'
 
 export default {
-  setup(_, { expose }) {
-    const contacts = ref([])
+  setup() {
     const search = ref('')
     const route = useRoute()
+    const contacts = ref([])
 
-    function loadContacts() {
+    // This always gets the latest contacts from localStorage when the route changes
+    watchEffect(() => {
+      route.fullPath // triggers the effect on route change
       contacts.value = getContacts()
-    }
-
-    // Load initially
-    loadContacts()
-
-    // Reload if the route is visited again (e.g., after adding a contact)
-    onBeforeRouteEnter((to, from, next) => {
-      next(vm => vm.loadContacts())
-    })
-
-    // Also reload if route changes while on the page
-    onBeforeRouteUpdate(() => {
-      loadContacts()
     })
 
     const filteredContacts = computed(() => {
@@ -40,9 +52,6 @@ export default {
         .sort((a, b) => a.lastName.localeCompare(b.lastName))
     })
 
-    // Expose to access from next()
-    expose({ loadContacts })
-
     return {
       search,
       filteredContacts
@@ -50,3 +59,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  max-width: 400px;
+}
+
+li {
+  padding: 0.5rem 0;
+  border-bottom: 1px solid #ddd;
+}
+</style>
